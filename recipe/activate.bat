56c934ecd5e3c0296e84819d351a26dd7ddd6fe2
H. Vetinari <h.vetinari@gmx.com>
2024-11-08 12:53:54 +1100

    distinguish include-path on windows (if using conda-build or not)

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -10,5 +10,13 @@ set "LD=lld-link.exe"
 set "AR=llvm-ar.exe"
 
 :: following https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.bat
-set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld -I%CONDA_PREFIX%\Library\include"
+set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld"
 set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%CONDA_PREFIX:\=/%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
+
+:: need to distinguish how we populate `-I` based on whether we're using conda build or not;
+:: if not, we want CONDA_PREFIX, but if yes, then that points to the build environment
+if not "%CONDA_BUILD%" == "" (
+    set "FFLAGS=%FFLAGS" -I%LIBRARY_INC%"
+) else (
+    set "FFLAGS=%FFLAGS" -I%CONDA_PREFIX%\Library\include"
+)
